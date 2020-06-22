940d374027a9633fa5cb6d9cc355aa3e14b5f394
vc <btk@zju.edu.cn>
2020-06-22 21:19:36 +0800

    修改下ad文件

--- Project/CODE/l_ad.c
+++ Project/CODE/l_ad.c
@@ -12,9 +12,9 @@ void l_init(void)
 
 void getl_once(void)
 {
-    l_data1 = adc_once(ADC_P10,ADC_10BIT);  //采集ADC_P10电压，精度10位
+    l_data1 = adc_once(ADC_P13,ADC_10BIT);  //采集ADC_P13电压，精度10位
 	l_data2 = adc_once(ADC_P11,ADC_10BIT); 	//采集ADC_P11电压，精度10位
-	l_data3 = adc_once(ADC_P13,ADC_10BIT);	//采集ADC_P13电压，精度10位
-	l_data4 = adc_once(ADC_P14,ADC_10BIT);	//采集ADC_P14电压，精度10位
+	l_data3 = adc_once(ADC_P14,ADC_10BIT);	//采集ADC_P14电压，精度10位
+	l_data4 = adc_once(ADC_P10,ADC_10BIT);	//采集ADC_P10电压，精度10位
 }
 
